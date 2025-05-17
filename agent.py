8a598a3081001f9b6635f9a15ac9f972b43f857b
Himel-Tasrif <himel35-1078@diu.edu.bd>
2025-05-18 01:21:31 +0600

    Updated file

--- agent.py
+++ agent.py
@@ -1,18 +1,3 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os, io, re
 import pandas as pd
 import streamlit as st
@@ -263,6 +248,7 @@ def main():
 
     with left:
         st.header("Data Analysis Agent")
+        st.markdown("###       -- Tasrif Nur Himel")
         st.markdown("<medium>Powered by <a href='https://build.nvidia.com/nvidia/llama-3_1-nemotron-ultra-253b-v1'>NVIDIA Llama-3.1-Nemotron-Ultra-253B-v1</a></medium>", unsafe_allow_html=True)
         file = st.file_uploader("Choose CSV", type=["csv"])
         if file:
